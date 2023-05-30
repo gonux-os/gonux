747f3da032a90e4792e3543bb2cfc12c787522ef
Toby Selway <tobyselway@outlook.com>
2023-05-31 00:10:30 +0100

    [Add] QEMU flags in run script

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 echo "Running qemu"
-qemu-system-x86_64 -kernel linux/arch/x86/boot/bzImage -initrd "out/rootfs.cpio.gz"
+qemu-system-x86_64 \
+    -kernel linux/arch/x86/boot/bzImage \
+    -initrd "out/rootfs.cpio.gz" \
+    -monitor stdio \
+    -usb
